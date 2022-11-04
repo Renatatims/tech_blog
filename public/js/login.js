fdb2c484d766a827e5538051da95be2a8465ee02
ReTims <renatatims@gmail.com>
2022-11-04 16:34:12 -0500

    login user + acess to user's dashboard + added individual hooks to User.bulcreate in seeds.js so password could be hashed

--- public/js/login.js
+++ public/js/login.js
@@ -1,28 +1,27 @@
 const loginFormHandler = async (event) => {
-    event.preventDefault();
-  
-    // Login form: username and password
-    const username = document.querySelector('#loginEmail').value.trim();
-    const password = document.querySelector('#loginPassword').value.trim();
-  
-    if (email && password) {
-      // POST request to the API endpoint
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ username, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        // If successful, redirect the browser to the dashboard page
-        document.location.replace('/dashboard');
-      } else {
-        alert(response.statusText);
-      }
-    }
-  };
+  event.preventDefault();
 
-  document
-  .querySelector('.login-input')
-  .addEventListener('submit', loginFormHandler);
- 
+  // Login form: username and password
+  const username = document.querySelector("#loginUsername").value.trim();
+  const password = document.querySelector("#loginPassword").value.trim();
+
+  if (username && password) {
+    // POST request to the API endpoint
+    const response = await fetch("/api/users/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      // If successful, redirect the browser to the dashboard page
+      document.location.replace("/dashboard");
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+document
+  .querySelector(".login-input")
+  .addEventListener("submit", loginFormHandler);
